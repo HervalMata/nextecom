73058f04f96f5a43df2c81a9f9305438cf4d0f3f
HervalMata <hervalmataamparo@gmail.com>
2023-11-05 02:19:39 -0300

    feat:art: nextecom - create connection to mondodb

---
+++ src/utils/dbConnect.js
@@ -0,0 +1,10 @@
+const { default: mongoose } = require("mongoose")
+
+const dbConnect = async () => {
+  if (mongoose.Connection.readyState >= 1) {
+    return;
+  }
+  mongoose.connect(process.env.DB_URI);
+}
+
+export default dbConnect;
