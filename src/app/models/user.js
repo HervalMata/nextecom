5245539168e642d5c25b26ecf395058bdc52d058
HervalMata <hervalmataamparo@gmail.com>
2023-11-05 14:40:24 -0300

    feat:art: nextecom - create user model

---
+++ src/app/models/user.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import uniqueValidator from "mongoose-unique-validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [
+        true,
+        "Por favor digite seu nome",
+      ],
+      trim: true,
+      minLength: 1,
+      maxLength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      minLength: 5,
+      maxLength: 20,
+    },
+    password: String,
+    role: {
+      type: String,
+      default: "user",
+    },
+    image: String,
+    resetCode: {
+      data: String,
+      expiresAt: {
+        type: Date,
+        default: () => new Date(Date.now() + 10 * 60 * 1000),
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.plugin(uniqueValidator);
+
+export default mongoose.models.User || mongoose.model("user", userSchema);
