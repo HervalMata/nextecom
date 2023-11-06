01bcf1db10608f11baf8a6f53d83461437ba1715
HervalMata <hervalmataamparo@gmail.com>
2023-11-05 22:09:22 -0300

    feat:art: nextecom - create register form

---
+++ src/app/register/page.js
@@ -0,0 +1,61 @@
+"use client"
+
+export default function Register() {
+  const [name, setName] = useState("Customer");
+  const [email, setEmail] = useState("customer@gmail.com");
+  const [password, setPassword] = useState("123456");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      console.table({ name, email, password });
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <main>
+      <div className="container">
+        <div className="row d-flex justify-content-center align-items-center vh-100">
+          <div className="col-lg-5 bg-light p-5 shadow">
+            <h2 className="mb-4">Cadastro</h2>
+
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control mb-4"
+                placeholder="Seu Nome"
+              />
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control mb-4"
+                placeholder="Seu Email"
+              />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control mb-4"
+                placeholder="Sua Senha"
+              />
+              <button
+                className="btn btn-primary btn-raised"
+                disabled={loading || !name || !email || !password}
+              >
+                {loading ? "Por favor espere..." : "Cadastrar"}
+              </button>  
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
